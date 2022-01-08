ef029fc8ab835e370b9304c520df2cbbb8af6775
WOOJIN, LEE <pappagallos@kakao.com>
2022-01-09 02:58:00 +0900

    유튜브 뉴스 채널 아무거나 Iframe 으로 넣고 폰트 사이즈 일부 수정 진행

--- components/video/Video.js
+++ components/video/Video.js
@@ -6,9 +6,10 @@ import styles from './scss/Video.module.scss';
 function Video() {
     return (
         <div className={styles.video_area}>
-            <video autoPlay playsInline controls>
-                <source src='/static/assets/video/demo.mp4' type='video/mp4' />
-            </video>
+            <iframe src='https://www.youtube.com/embed/0GN8t2u3flc' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen></iframe>
+            {/* <video autoPlay playsInline controls loop>
+                <source src='/static/assets/video/play_video.mp4' type='video/mp4' />
+            </video> */}
         </div>
     )
 }
