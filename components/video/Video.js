10f532c3e7ebfc1526c01b256d58f3771544e7c6
WOOJIN, LEE <pappagallos@kakao.com>
2022-01-09 13:35:16 +0900

    UI 일부 수정 및 Iframe 유튜브 검정 배경화면 보이는 현상 없애기 위해 scale 스타일 지정

--- components/video/Video.js
+++ components/video/Video.js
@@ -6,7 +6,7 @@ import styles from './scss/Video.module.scss';
 function Video() {
     return (
         <div className={styles.video_area}>
-            <iframe src='https://www.youtube.com/embed/0GN8t2u3flc' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen></iframe>
+            <iframe src='https://www.youtube.com/embed/0GN8t2u3flc?autoplay=1&mute=1' title='YouTube video player' frameBorder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowFullScreen></iframe>
             {/* <video autoPlay playsInline controls loop>
                 <source src='/static/assets/video/play_video.mp4' type='video/mp4' />
             </video> */}
