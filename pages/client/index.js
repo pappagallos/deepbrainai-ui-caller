05ee35d1265618125296dd7279c0021e9861082e
WOOJIN, LEE <pappagallos@kakao.com>
2022-01-09 23:12:09 +0900

    WebSocket 연동 및 소켓 통신 테스트 완료

--- pages/client/index.js
+++ pages/client/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 // components
 import Header from '../../components/header/Header';
@@ -10,6 +11,21 @@ import Footer from '../../components/footer/Footer';
 import styles from '../../styles/ClientPage.module.scss';
 
 const ClientPage = () => {
+    // const [socket, setSocket] = useState();
+
+    useEffect(() => {
+        const socket = io('http://localhost:8005', {
+            reconnectionDelayMax: 10000,
+        });
+        // setSocket(connSocket);
+
+        socket.on('connect', () => {
+            console.log(socket.connected);
+        });
+
+        socket.emit('message', { message: '테스트123' });
+    }, []);
+
     return (
         <>
             <header>
